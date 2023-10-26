a878319951d21fb37059202a4389daef66933d4b
Mustafa Kemal Gördesli <m.kemalgordesli@gmail.com>
2023-10-26 10:48:32 +0300

    Add user store and user fetch thunk

---
+++ src/stores/user.ts
@@ -0,0 +1,31 @@
+import { createSlice, createAsyncThunk } from '@reduxjs/toolkit';
+import axios from 'axios';
+import { getToken } from '@/lib/token';
+
+const initialState = {
+    user: {},
+};
+
+export const fetchUser = createAsyncThunk('user/fetchUser', async () => {
+    const token = getToken();
+    if (!token) return {};
+    const res = await axios.get(
+        process.env.NEXT_PUBLIC_API_BASE + '/api/users/getbytoken',
+        {
+            headers: { authorization: 'Bearer ' + token },
+        }
+    );
+    if (res?.data?.success) return res.data?.data;
+    return {};
+});
+
+export const { reducer, actions } = createSlice({
+    name: 'user',
+    initialState,
+    reducers: {},
+    extraReducers: (builder) => {
+        builder.addCase(fetchUser.fulfilled, (state, action) => {
+            state.user = action.payload;
+        });
+    },
+});
